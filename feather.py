0809899c66c3e13f39d20e36a2e03f49152890d4
Space Walker <spacedoesrs@gmail.com>
2022-11-23 18:19:28 +0100

    hack to make snapshots prior to 13w16a not available

--- feather.py
+++ feather.py
@@ -6,6 +6,9 @@ MAPPINGS_DIR = 'mappings'
 GRADLE_TASKS = ['clean', 'feather', 'build', 'javadoc', 'javadocJar', 'checkMappings', 'mapCalamusJar', 'mapNamedJar', 'decompileCFR', 'decompileQuiltflower', 'decompileProcyon', 'publish', 'separateMappings', 'insertMappings', 'propagateMappingsDown', 'propagateMappingsUp', 'propagateMappings']
 GRADLEW = 'gradlew' if os.name == 'nt' else './gradlew'
 
+# hack to make some versions unavailable even though mappings exist for them
+UNAVAILABLE_VERSIONS = ['12w32a','12w34a','12w34b','12w36a','12w37a','12w38a','12w38b','12w39a','12w39b','12w40a','12w40b','12w41a','12w41b','12w42a','12w42b','12w49a','12w50a','12w50b','13w01a','13w01b','13w02a','13w02b','13w03a','13w04a','13w05a','13w05b','13w06a','13w07a','13w09a','13w09b','13w09c','13w10a','13w10b','13w11a']
+
 def main():
     possible_versions = find_minecraft_versions()
     versions = []
@@ -17,7 +20,10 @@ def main():
         arg = args[i]
         
         if arg in possible_versions:
-            versions.append(arg)
+            if arg in UNAVAILABLE_VERSIONS:
+                raise Exception('unavailable version ' + arg + '!');
+            else:
+                versions.append(arg)
         elif arg in GRADLE_TASKS:
             tasks.append(arg)
         else:
@@ -32,7 +38,7 @@ def main():
             else:
                 raise Exception('no minecraft version given!')
         else:
-            versions = possible_versions
+            versions = [version for version in possible_versions if version not in UNAVAILABLE_VERSIONS]
     if len(tasks) == 0:
         raise Exception('no gradle tasks given!')
     
