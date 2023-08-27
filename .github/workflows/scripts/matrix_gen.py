fce55bd32bfb26e54b0d5cb3f67dcd2e750ac2b7
kahzerx <kahzerx@tutanota.com>
2023-08-28 00:51:48 +0200

    version parser

--- .github/workflows/scripts/matrix_gen.py
+++ .github/workflows/scripts/matrix_gen.py
@@ -1,9 +1,34 @@
 import json
+import math
 import os
+from typing import Generator
+
+
+def find_minecraft_versions() -> Generator[str, str, None]:
+    for filename in os.listdir("mappings"):
+        if filename.endswith(".tiny"):
+            yield filename.removesuffix(".tiny")
+        elif filename.endswith(".tinydiff"):
+            if len(pair := filename.removesuffix(".tinydiff").split("#")) == 2:
+                yield pair[-1]
+
+
+def split_in_jobs(n_jobs: int, versions: list[str]) -> list[dict[str, list[str]]]:
+    versions_per_job: int = math.ceil(len(versions) / n_jobs)
+    start: int = 0
+    result: list[dict[str, list[str]]] = []
+    for i in range(n_jobs):
+        result.append({"version_range": versions[start:start+versions_per_job]})
+        start += versions_per_job
+    return result
 
 
 def main():
-    version_split: str = json.dumps({"include": [{"version_range": ["1.1", "1.2", "1.3"]}, {"version_range": ["1.4", "1.5", "1.6"]}]})
+    os.chdir("/home/kahzerx/Documents/black-lotus/feather-mappings")
+    minecraft_versions: list[str] = list(set(find_minecraft_versions()))
+    print(minecraft_versions)
+    jobs: list[dict[str, list[str]]] = split_in_jobs(19, minecraft_versions)
+    version_split: str = json.dumps({"include": jobs})
     with open(os.environ["GITHUB_OUTPUT"], "w") as f:
         f.write(f"versions={version_split}\n")
     print(os.getenv("GITHUB_OUTPUT"))
