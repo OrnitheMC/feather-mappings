e820e0f19adee4577d93be32746f73f7d08a5f22
kahzerx <kahzerx@tutanota.com>
2023-08-28 00:34:28 +0200

    other way of writing env

--- .github/workflows/scripts/matrix_gen.py
+++ .github/workflows/scripts/matrix_gen.py
@@ -4,7 +4,8 @@ import os
 
 def main():
     version_split: str = json.dumps({"include": [{"version_range": ["1.1", "1.2", "1.3"]}, {"version_range": ["1.4", "1.5", "1.6"]}]})
-    os.environ["GITHUB_OUTPUT"] = f"versions={version_split}"
+    with open(os.environ["GITHUB_OUTPUT"], "w") as f:
+        f.write(f"versions={version_split}\n")
     print(os.getenv("GITHUB_OUTPUT"))
 
 
